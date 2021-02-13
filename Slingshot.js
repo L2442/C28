0ddefbe1f099a75052f792e829f407f78ad79917
L2442 <swastikshandilya11@gmail.com>
2021-02-13 09:42:19 +0530

    Angry birds

---
+++ Slingshot.js
@@ -0,0 +1,30 @@
+class Slingshot{
+    constructor(body1, point1){
+        var options = {
+            bodyA: body1,
+            pointB: point1,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.sling = Constraint.create(options);
+        console.log(this.sling);
+        World.add(world, this.sling);
+    }
+
+    fly(){
+        this.sling.bodyA = null;    
+        }
+
+    display(){
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.sling.pointB;
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+       
+    }
+
+   
+    
+}
